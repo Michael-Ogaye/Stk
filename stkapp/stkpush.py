613e0c3d95e13dabd666b218b6b00dfdb9749ea3
Michael-Ogaye <hogaeymic@gmail.com>
2023-02-12 01:17:48 +0300

    lipa na mpesa authtoken method

--- stkapp/stkpush.py
+++ stkapp/stkpush.py
@@ -1,4 +1,8 @@
 import requests
+import base64
+from requests.auth import HTTPBasicAuth
+from datetime import datetime
+import json
 
 class StkPush:
     def __init__(self,consumer_key,consumer_secret,pass_key):
@@ -6,4 +10,15 @@ class StkPush:
         self.consumer_key=consumer_key
         self.pass_key=pass_key
 
-    def auth_token:
+    def auth_token(self):
+        authm=HTTPBasicAuth(self.consumer_key,self.consumer_secret)
+        
+        url_auth='https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials'
+        try:
+
+           res=requests.get(url_auth,auth=authm)
+
+        except:
+            res=requests.get(url_auth,auth=authm,verify=False)
+
+        return res.json()['access_token']
