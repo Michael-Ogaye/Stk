06542fd7d7b97d3df33520b28fa3a44a048c186e
Michael-Ogaye <hogaeymic@gmail.com>
2023-02-12 01:34:10 +0300

    creating a password from short code and timestamps

--- stkapp/stkpush.py
+++ stkapp/stkpush.py
@@ -22,3 +22,22 @@ class StkPush:
             res=requests.get(url_auth,auth=authm,verify=False)
 
         return res.json()['access_token']
+
+    def timestamp():
+      unformatted_time = datetime.now()
+      formatted_time = unformatted_time.strftime("%Y%m%d%H%M%S")
+
+      
+      return formatted_time
+    
+    def generate_password(self):
+        f_time=self.timestamp()
+        data_to_encode = (
+        self.shortCode + self.pass_key +f_time
+        )
+
+        encoded_string = base64.b64encode(data_to_encode.encode())
+    
+        decoded_password = encoded_string.decode("utf-8")
+
+        return decoded_password
